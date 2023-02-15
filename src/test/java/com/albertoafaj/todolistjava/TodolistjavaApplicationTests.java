776f5a799b1a8a6fded39c7fcef2f71bfafcf5a4
Alberto Araújo <albertoafaj@hotmail.com>
2023-02-15 15:21:56 -0300

    initial commit

---
+++ src/test/java/com/albertoafaj/todolistjava/TodolistjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertoafaj.todolistjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
