9beda52e5ffd53a4b85d1453dbf940ec9cb3a31a
Alberto Araújo <albertoafaj@hotmail.com>
2023-02-16 16:25:06 -0300

    feat: create task save method

--- src/main/java/com/albertoafaj/todolistjava/service/TaskService.java
+++ src/main/java/com/albertoafaj/todolistjava/service/TaskService.java
@@ -1,5 +1,21 @@
 package com.albertoafaj.todolistjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.albertoafaj.todolistjava.model.Task;
+import com.albertoafaj.todolistjava.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+  @Autowired
+  private TaskRepository taskRepository;
+
+  public Task createTask(Task task) {
+    return taskRepository.save(task);
+  }
 
 }
